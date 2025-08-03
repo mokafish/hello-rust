1a6e8b8c1f56b06ded36237861676e083bb7d4ee
mokafish <mokafish@users.noreply.github.com>
2025-08-03 19:21:56 +0800

    feat: enums (todo)

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,18 +1,20 @@
 // mod s01_data_type;
 // mod s02_function;
 // mod s03_control_flow;
-mod s04_ownership;
-mod s05_references;
-mod s06_slices;
-mod s07_structs;
+// mod s04_ownership; 
+// mod s05_references;
+// mod s06_slices;
+// mod s07_structs;
+mod s08_enums;
 
 fn main() {
     println!("Hello, world!");
     // s01_data_type::main();
     // s02_function::main();
     // s03_control_flow::main();
-    s04_ownership::main();
-    s05_references::main();
-    s06_slices::main();
-    s07_structs::main();
+    // s04_ownership::main();
+    // s05_references::main();
+    // s06_slices::main();
+    // s07_structs::main();
+    s08_enums::main();
 }
---
+++ demo/src/s08_enums.rs
@@ -0,0 +1,4 @@
+pub fn main(){
+    println!("--- s08_enums ---");
+    println!("Todo ...");
+}
