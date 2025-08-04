ce21970481ac99d04c5c7ce4d443f9a390e8318c
mokafish <mokafish@users.noreply.github.com>
2025-08-04 14:20:06 +0800

    feat: support ignore_case flag

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -8,14 +8,13 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("解析参数时遇到问题：{err}");
+        eprintln!("解析参数时遇到问题：{err}");
         process::exit(1);
     });
 
     println!("在文件 {} 中检索：{}", config.file_path, config.query);
     if let Err(e) = run(config) {
-        println!("应用程序错误：{e}");
+        eprintln!("应用程序错误：{e}");
         process::exit(1);
     }
 }
-
